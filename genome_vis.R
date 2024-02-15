1f95111898766940fcebee6332043aae25e2c49b
Nancy Scott <scot0854@umn.edu>
2024-02-15 09:26:26 -0600

    Added genome features to plot and moved chr labels to file.

--- genome_vis.R
+++ genome_vis.R
@@ -10,7 +10,7 @@
 ##
 ## Email: scot0854@umn.edu
 ## ---------------------------
-## Notes: Adapted significantly from https://github.com/berman-lab/ymap
+## Notes: Adapted from https://github.com/berman-lab/ymap
 ## and https://github.com/stajichlab/C_lusitaniae_popseq.
 ## Inputs are tab-delim files from samtools depth and samtools mpileup, with headers.
 ## This script uses those headers, so change carefully.
@@ -21,63 +21,68 @@
 #!/usr/bin/env Rscript
 args = commandArgs(trailingOnly = TRUE)
 
-# input file variables
+# Input files
 read_depth_file <- args[1] # To run interactively, replace args[1] with "path/to/your_depth_file.txt"
 snp_file <- args[2] # or "path/to/your_putative_SNPs.txt"
 sample_id <- args[3] # or "YourID"
+feature_file <- args[4] # or "path/to/features.txt"
+label_file <- args[5] # or "path/to/chr_labels.txt"
 mito <- "Ca19-mtDNA" # scaffold ID for subsetting
 
 ## ---------------------------
-# load packages
+# Load packages
 library(RcppRoll)
 library(tidyverse)
 library(ggplot2)
 library(writexl)
+library(readxl)
 
 ## ---------------------------
-# data-wrangling variables
+# Input variables
 window <- 5000 # size of window used for rolling mean and snp density
-ploidy <- 2 # will multiply this by relative read depth for copy number
+ploidy <- 2
 
-# plotting variables
-chr_ids <- c("Ca21chr1_C_albicans_SC5314"="Chr1",
-             "Ca21chr2_C_albicans_SC5314"="Chr2",
-             "Ca21chr3_C_albicans_SC5314"="Chr3",
-             "Ca21chr4_C_albicans_SC5314"="Chr4",
-             "Ca21chr5_C_albicans_SC5314"="Chr5",
-             "Ca21chr6_C_albicans_SC5314"="Chr6",
-             "Ca21chr7_C_albicans_SC5314"="Chr7",
-             "Ca21chrR_C_albicans_SC5314"="ChrR") # manual x-axis labels overwrite input scaffold names in final plot
-y_axis_labels <- c(1,2,3,4)  # manual y-axis labels for now, to remove "0" from axis
+y_axis_labels <- c(1,2,3,4)  # manual y-axis labels, adjust as needed
 inter_chr_spacing <- 150000 # size of space between chrs
-snp_low <- "white"  # snp LOH colors, plot function uses 2-color gradient scale
-snp_high <- "black"  # snp LOH colors, plot function uses 2-color gradient scale
-cnv_color <- "steelblue4"  # copy number color
-ploidy_multiplier <- 2  # this number multiplied by ploidy sets the max-y scale
-chrom_outline_color <- "gray15"  # color of chromosome outlines
-chrom_line_width <- 0.2  # line width of chromosome outlines
 
-# output variables
-save_dir <- "plots/" # path with trailing slash, or just "" to save where you are
-ref <- "sc5314" # short label for generating file name or "" to leave out
+save_dir <- "plots/" # path with trailing slash or "" to save locally
+ref <- "sc5314" # label for file name or "" to leave out
+
+# Plotting variables
+# For overwriting scaffold names in final plot
+chr_ids <- scan(label_file, what = character())
+
+# SNP LOH colors, plot function uses 2-color gradient scale
+snp_low <- "white"
+snp_high <- "black"
+# Copy number color
+cnv_color <- "dodgerblue4"
+
+# Set the max y-axis (ploidy_multiplier * ploidy)
+ploidy_multiplier <- 2
+
+# Color of chromosome outlines
+chrom_outline_color <- "gray15"
+
+# Line width of chromosome outlines
+chrom_line_width <- 0.2
 
 ## ---------------------------
 # Base R doesn't have a mode calculation
-# nice to compare this to median but not essential
 Modes <- function(x) {
   ux <- unique(x)
   tab <- tabulate(match(x, ux))
   ux[tab == max(tab)]
 }
 
-# reduce noise of allele frequency for plotting
+# Reduce noise of allele frequency for plotting
 read_freq <- function(x){
   x=ifelse(x>0.95, NA, ifelse(x>=0.05, x , NA))
   return(x)
 }
 
 ## ---------------------------
-# relative copy number calcs from samtools depth input
+# Relative copy number calcs from samtools depth input
 genome_raw <- read.table(read_depth_file, header = TRUE)
 genome_raw <- genome_raw %>%
   filter(chr != mito)
@@ -94,23 +99,22 @@ subset_chr_median <- chr_median %>%  # modest filtering to avoid aneuploidy skew
 
 genome_median <- median(subset_chr_median$chr_med)  # filtered median used to calculate relative depth
 
-# reshaping dataframe for future plotting
+# Reshape dataframe for future plotting
 genome_window <- genome_raw %>%
   group_by(chr, index=consecutive_id(chr)) %>%
   reframe(position=unique((pos %/% window)*window+1))
 
-# more plotting stuff
+# More plotting details
 genome_window <- genome_window %>%
   group_by(index) %>%
+  arrange(index) %>%
   mutate(chr_length = max(position))
 
-# for plotting
 chrs <- as.vector(unique(genome_window$chr_length))
 chr_plot <- c()
 for(i in 1:length(chrs)){chr_plot[i] <- sum(chrs[1:i-1])}
 
-# finally calculate relative depth and copy number
-# plus more plotting details!
+# Finally calculate relative depth and copy number, plus more plotting
 genome_depth <- genome_raw %>%
   group_by(chr, index=consecutive_id(chr)) %>%
   filter(pos %in% genome_window$position) %>%
@@ -136,9 +140,9 @@ genome_snp <- genome_snp %>%
   mutate(allele_1 = pmax(A_freq, T_freq, G_freq, C_freq, na.rm = TRUE)) %>%
   mutate(allele_2 = pmin(A_freq, T_freq, G_freq, C_freq, na.rm = TRUE))
 
-# set a limit for allele frequency for heterozygosity and sums those within limit, per bin
+# Set a limit for het allele frequency and sums those within limit, per bin
 # see ymap paper and github: 25-75% are limits for diploids
- gaf <- genome_snp %>%
+gaf <- genome_snp %>%
   group_by(chr, snp_bin) %>%
   summarize(snp_count = sum((A_freq >= (1/copy_number)*0.5 & A_freq <=(1-(1/copy_number)*0.5)) |
                               (T_freq >= (1/copy_number)*0.5 & T_freq <=(1-(1/copy_number)*0.5)) |
@@ -146,28 +150,45 @@ genome_snp <- genome_snp %>%
                               (C_freq >= (1/copy_number)*0.5 & C_freq <=(1-(1/copy_number)*0.5)), na.rm = TRUE))
 
 ## ---------------------------
-# final dataframe of joined copy number, snps, and plotting positions per window
+# Final dataframe of joined copy number, snps, and plotting positions per window
 genome_depth <- genome_depth %>%
   left_join(gaf, by=c("chr", "pos"="snp_bin"))
 
 ## ---------------------------
-# little dataframe to plot chromosome outlines
+# Small dataframes for chrom. outlines and features
 chroms <- genome_depth %>%
   group_by(index) %>%
   summarise(xmin=min(plot_pos), xmax=max(plot_pos), ymin=0, ymax=Inf)
 
-# tick marks to center chromosome ID label
-ticks <- tapply(genome_depth$plot_pos, genome_depth$index, quantile, probs = 0.5)
+features <- read_tsv(feature_file, show_col_types = FALSE)
+
+features <- features %>%
+     group_by(chr, index=consecutive_id(chr)) %>%
+     left_join(chroms, by=join_by(index))
+
+features <- features %>%
+     mutate(plot_start = start + xmin, plot_end = end + xmin)
+
+# Tick marks to center chromosome ID label
+ticks <- tapply(genome_depth$plot_pos, genome_depth$index, quantile, probs =
+                0.5, na.remove = TRUE)
 
 ## ---------------------------
-# plot linear genome
+# Plot linear genome
 p <- ggplot(genome_depth) +
   scale_color_gradient(low=snp_low,high=snp_high, na.value = "white", guide = "none") +
   geom_segment(aes(x = plot_pos, y = 0, color = snp_count, xend = plot_pos, yend = Inf)) +
-  geom_segment(aes(x = plot_pos, y = ifelse(copy_number <= ploidy*ploidy_multiplier, copy_number, Inf),
+  geom_segment(aes(x = plot_pos,
+                   y = ifelse(copy_number <= ploidy*ploidy_multiplier, copy_number, Inf),
                    xend = plot_pos, yend = ploidy), alpha = 0.9, color = cnv_color) +
   geom_rect(data=chroms, aes(group=index, xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax),
-            linewidth = chrom_line_width, fill = NA, colour = chrom_outline_color, linejoin = "round", inherit.aes = FALSE) +
+            linewidth = chrom_line_width, fill = NA,
+            colour = chrom_outline_color, linejoin = "round", inherit.aes = FALSE) +
+  geom_point(data = features, size = 2,
+               aes(group=index, x=plot_start, y=ymin, shape = Feature, fill = Feature),
+               position = position_nudge(y=0.065)) +
+  scale_fill_manual(values = c("white", "grey26", "deepskyblue")) +
+  scale_shape_manual(values = c(24,21,22)) +
   ylab(sample_id) +
   scale_x_continuous(name = NULL, expand = c(0, 0), breaks = ticks, labels=chr_ids) +
   scale_y_continuous(limits = c(0, ploidy*ploidy_multiplier), breaks = y_axis_labels) +
@@ -175,13 +196,13 @@ p <- ggplot(genome_depth) +
   theme(plot.title = element_text(size = 12, hjust = 0.5),
         axis.ticks = element_line(color = NA),
         axis.line = element_blank(),
-        axis.text.y = element_text(size = 12),
+        axis.title.y = element_text(size = 12),
         axis.text.x = element_text(size=12))
 
 ## ---------------------------
-# save plot as jpg. Height to width ratio is eyeballed for now
-ggsave(sprintf("%s%s_%s_%s_%sbp.jpg", save_dir, Sys.Date(), sample_id, ref, window),
-       p, width = 18, height = 1.7, units = "in")
+# Save plot
+ggsave(sprintf("%s%s_%s_%s_%sbp.png", save_dir, Sys.Date(), sample_id, ref, window),
+       p, width = 18, height = 1.7, units = "in", device = png, dpi = 300, bg = "white")
 
 ## ---------------------------
 # Save dataframes as excel
@@ -193,13 +214,14 @@ outfiles <- list(plotting_data=genome_depth,
 write_xlsx(outfiles, path = sprintf("%s%s_%s.xlsx", save_dir, Sys.Date(), sample_id))
 
 ## ---------------------------
-# plot and save allele freqs per chr
+# Plot allele freqs per chr
 allele_freq_histo <-   ggplot(genome_snp) +
   geom_histogram(aes(allele_1),bins=200) +
   geom_histogram(aes(allele_2), bins = 200) +
   facet_wrap(~as.factor(chr), ncol=8, labeller = as_labeller(chr_ids)) +
   xlab("Allele frequency") +
-  ylab(sample_id)
+  ylab(sample_id) +
+  theme_classic()
 
-ggsave(sprintf("%s%s_%s_%s_allele_freq.jpg", save_dir, Sys.Date(), sample_id, ref),
-       allele_freq_histo, width = 18, height = 1.7, units = "in")
+ggsave(sprintf("%s%s_%s_%s_allele_freq.png", save_dir, Sys.Date(), sample_id, ref),
+       allele_freq_histo, width = 18, height = 1.7, units = "in", device = png, dpi=300, bg = "white")
