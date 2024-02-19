14a0cef9ab6c4cd036b75efb6158018105394384
Nancy Scott <scot0854@umn.edu>
2024-02-19 15:41:09 -0600

    Add plot of combined relative copy number

--- LOH_heatmap.R
+++ LOH_heatmap.R
@@ -1,7 +1,7 @@
 ## ---------------------------
-## Script name: snp_heatmap.R
+## Script name: LOH_heatmap.R
 ##
-## Purpose: Combine het SNP counts and generate heatmap across chrs 
+## Purpose: Combine het SNP counts and generate heatmap across chrs
 ##
 ## Author: Nancy Scott
 ##
@@ -12,10 +12,10 @@
 #!/usr/bin/env Rscript
 #args = commandArgs(trailingOnly = TRUE)
 
-# Input vars 
-spreadsheet_list <- "genome_sheets.txt"
-sample_list <- "raxml_midpoint_phylogeny.csv"
-save_dir <- "" # path with trailing slash or "" to save locally
+# Input vars
+spreadsheet_list <- "~/umn/data/metadata/Calbicans_snp-depth_paths.txt"
+sample_list <- "~/umn/data/metadata/Calbicans_MEC_raxml_midpoint_tips.csv"
+save_dir <- "~/umn/images/Calbicans/"
 
 ## ---------------------------
 # Load packages
@@ -26,61 +26,65 @@ library(paletteer)
 library(writexl)
 
 ## ---------------------------
-# Concatenate snp counts and tidy data
+# Read in SNP counts for all samples
 snp_files <- scan(spreadsheet_list, what=character())
 
-genome_snp <- read_xlsx(snp_files[1]) %>% 
+genome_snp <- read_xlsx(snp_files[1]) %>%
   select(index, pos, snp_count)
 names(genome_snp)[names(genome_snp)=="snp_count"] <-str_extract(snp_files[1], "AMS[:digit:]+|MEC[:digit:]+")
 
 for(i in 2:100){
-  new_snp <- read_xlsx(snp_files[i]) %>% 
+  new_snp <- read_xlsx(snp_files[i]) %>%
     select(index, pos, snp_count)
   names(new_snp)[names(new_snp)=="snp_count"] <-str_extract(snp_files[i], "AMS[:digit:]+|MEC[:digit:]+")
-  
-  genome_snp <- genome_snp %>% 
+
+  genome_snp <- genome_snp %>%
     left_join(new_snp, by = join_by(index,pos))
 }
 
-# Plotting:regular intervals for x-axis
+# Plotting: regular intervals for x-axis
 genome_snp$x_num <- seq.int(nrow(genome_snp))
 
-snp_again <- genome_snp %>% 
+# Finish tidying data
+snp_again <- genome_snp %>%
   pivot_longer(names_to = "sample", values_to = "snp_count", cols=-c(index,pos, x_num))
 
-# Chr outlines and tick marks
-chrs <- genome_snp %>% 
-  group_by(index) %>% 
+# Plotting: chr outlines and tick marks
+chrs <- genome_snp %>%
+  group_by(index) %>%
   summarise(border_start=min(x_num),
             border_stop=max(x_num),
             tick=min(x_num) + (max(x_num)-min(x_num))/2
             )
 
-# To sort samples by tree order
+# Sort samples by tree order
 sample_order <- read_csv(sample_list, show_col_types = FALSE)
 
 # Plot
 p <- snp_again %>%
-  mutate(clustered_samples = fct_relevel(sample, rev(sample_order$sample))) %>% 
+  mutate(clustered_samples = fct_relevel(sample, rev(sample_order$sample))) %>%
     ggplot(aes(x=x_num, y=clustered_samples, fill=snp_count))+
-    scale_fill_gradient(low = "white", high="#9C0824", na.value = "white", name="Heterozygous SNPs\n per 5kb window") +
+    #scale_fill_gradient2(low = "white", mid="darkgoldenrod2", high = "dodgerblue3",
+     #                    midpoint= 80, na.value = "white",
+       #                  name="Heterozygous SNPs\n per 5kb window") +
+    scale_fill_gradient(na.value = "white", high = "#800000", low = "white",
+                      name="Heterozygous SNPs\n per 5kb window") +
     geom_tile() +
     theme_minimal() +
     geom_rect(data=chrs, aes(group=index, xmin=border_start, xmax=border_stop, ymin=0.5, ymax=Inf),
                              fill=NA, inherit.aes=FALSE,colour = "grey26", linejoin = "round") +
-    scale_x_continuous(expand = c(0,0), 
+    scale_x_continuous(expand = c(0,0),
                        breaks = chrs$tick,
                        labels=c("Chr1", "Chr2", "Chr3", "Chr4", "Chr5", "Chr6", "Chr7", "ChrR"))+
-    theme(#axis.text.x = element_blank(),
-        axis.title.x = element_blank(),
+    theme(axis.title.x = element_blank(),
         axis.title.y = element_blank(),
         axis.text.y = element_text(size=6.5),
         legend.title = element_text(size=10)
         )
 
-# what are the axis positions per sample?
+# What are the axis positions per sample?
 #details <- ggplot_build(p)
 
-ggsave("MEC_Calbicans_LOH_heatmap.png", p, device=png, dpi=300, bg="white",
+ggsave(paste0(save_dir,Sys.Date(),"_MEC_Calbicans_LOH_heatmap.png"), p, device=png, dpi=300, bg="white",
        width = 10, height = 7.5, units="in")
 
---
+++ group_copy_number_vis.R
@@ -0,0 +1,130 @@
+### ---------------------------
+## Script name: group_copy_number_vis.R
+##
+## Purpose of script: plot all relative copy number data for a group of samples
+##
+## Author: Nancy Scott
+##
+## Date Created: 2024-02-19
+##
+## Email: scot0854@umn.edu
+## ---------------------------
+# Input file variables
+spreadsheet_list <- "~/umn/data/metadata/Calbicans_snp-depth_paths.txt"
+feature_file <- "ref_genome_files/Calbicans_SC5314_A21_plotting_features.txt"
+label_file <-  "ref_genome_files/Calbicans_SC5314_A21_chr_labels.txt"
+patient_data <- "~/umn/data/metadata/2022_Calbicans_sorted_patient_pop.csv"
+
+## ---------------------------
+# Load packages
+library(readxl)
+library(tidyverse)
+library(ggplot2)
+library(paletteer)
+
+## ---------------------------
+# Set vars
+ploidy <- 2
+
+y_axis_labels <- c(1,2,3,4,5,6)  # manual y-axis labels, adjust as needed
+inter_chr_spacing <- 150000 # size of space between chrs
+
+save_dir <- paste0("images/Calbicans/",Sys.Date(),"_combined_CN_plots/") # path with trailing slash, or  "" to save locally
+ref <- "sc5314" # short label for file name or "" to leave out
+
+patient_colors <- c(paletteer_d("colorBlindness::paletteMartin"))
+
+# Plotting variables
+# X-axis labels overwrite input scaffold names in final plot
+chr_ids <- scan(label_file, what = character())
+
+ploidy_multiplier <- 3  # this multiplied by ploidy sets the max-y scale
+
+chrom_outline_color <- "gray15"  # color of chromosome outlines
+
+chrom_line_width <- 0.2  # line width of chromosome outlines
+
+# Read in patient metadata
+pop.data <- read.table(patient_data,
+                       sep = ",",
+                       header = TRUE)
+
+# Read in depth data for all isolates
+depth_files <- scan(spreadsheet_list, what=character())
+
+genome_depth <- read_xlsx(depth_files[1]) %>%
+  select(chr, index, pos, plot_pos, copy_number)
+names(genome_depth)[names(genome_depth)=="copy_number"] <-str_extract(depth_files[1], "AMS[:digit:]+|MEC[:digit:]+")
+
+for(i in 2:100){
+  new_depth <- read_xlsx(depth_files[i]) %>%
+    select(chr, index, pos, plot_pos, copy_number)
+  names(new_depth)[names(new_depth)=="copy_number"] <-str_extract(depth_files[i], "AMS[:digit:]+|MEC[:digit:]+")
+
+  genome_depth <- genome_depth %>%
+    left_join(new_depth, by = join_by(chr,index,pos, plot_pos))
+}
+
+genome_depth <- rename(genome_depth, c("MEC103_2"="MEC103"))
+
+genome_depth <- genome_depth %>%
+  pivot_longer(names_to = "sample", values_to = "copy_number", cols=-c(chr,index,pos,plot_pos))
+
+genome_depth <- genome_depth %>% inner_join(pop.data, by=join_by(sample))
+
+genome_depth <- genome_depth%>%
+  mutate(copy_number = ifelse(sample %in% c("MEC185", "MEC324"), copy_number +1, copy_number)) %>%
+  arrange(ploidy_change)
+
+## ---------------------------
+# Small dataframes for chrom. outlines and features
+chroms <- genome_depth %>%
+  group_by(index) %>%
+  summarise(xmin=min(plot_pos), xmax=max(plot_pos), ymin=0, ymax=Inf)
+
+features <- read_tsv(feature_file, show_col_types = FALSE)
+
+features <- features %>%
+  group_by(chr, index=consecutive_id(chr)) %>%
+  left_join(chroms, by=join_by(index))
+
+features <- features %>%
+  mutate(plot_start = start + xmin, plot_end = end + xmin)
+
+# Tick marks to center chromosome ID label
+ticks <- tapply(genome_depth$plot_pos, genome_depth$index, quantile, probs =
+                  0.5, na.remove = TRUE)
+
+## ---------------------------
+# Plot linear genome
+p <- genome_depth %>%
+  group_by(sample) %>%
+  ggplot() +
+  geom_point(aes(x = plot_pos,
+                 y = copy_number,
+                 color=as.factor(ploidy_change)),alpha = 0.8, size=0.3, shape=20,
+             show.legend = FALSE) +
+  scale_color_manual(values=patient_colors) +
+  geom_point(data = features, size = 2,
+             aes(group=index, x=plot_start, y=ymin, shape = Feature),# fill = Feature),
+             position = position_nudge(y=0.07)) +
+  #scale_fill_manual(values = c("white", "grey26", "deepskyblue")) +
+  scale_shape_manual(values = c(17,19,15), guide = guide_legend(title = NULL)) +
+  ylab('Relative copy number') +
+  geom_rect(data=chroms, aes(group=index, xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax),
+            linewidth = chrom_line_width, fill = NA,
+            colour = chrom_outline_color, linejoin = "round", inherit.aes = FALSE) +
+  scale_x_continuous(name = NULL, expand = c(0, 0), breaks = ticks, labels=chr_ids) +
+  scale_y_continuous(limits = c(0, ploidy*ploidy_multiplier), breaks = y_axis_labels) +
+  theme_classic() +
+  theme(plot.title = element_text(size = 12, hjust = 0.5),
+        axis.ticks = element_line(color = NA),
+        axis.line = element_blank(),
+        axis.text.y = element_text(size = 12),
+        axis.text.x = element_text(size=12),
+        legend.position = "bottom")
+
+## ---------------------------
+# Save plot
+ggsave("try1.png",
+       p, width = 12, height = 2.7, units = "in", device = png, dpi = 300, bg = "white")
